94a42636dec8d59532e1804561b10e9904e401e2
yibudak <yigit@altinkaya.com>
2025-02-13 08:07:48 +0000

    [REF] postmark_connector: code optimization and mail_tracking implementation

--- postmark_connector/models/mail_message.py
+++ postmark_connector/models/mail_message.py
@@ -1,36 +1,24 @@
-from odoo import models, fields, _
+# Copyright (C) 2025 Ahmet Yiğit Budak (https://github.com/yibudak)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from odoo import fields, models
 
 
 class MailMessage(models.Model):
     _inherit = "mail.message"
-    _description = "Adds 'opened' state to mail.message through postmark integration"
+    _description = "Adds states to mails through postmark integration"
 
-    postmark_api_state = fields.Selection(
-        selection=[
-            ("error", "Error"),
-            ("sent", "Sent"),
-            ("open", "Opened"),
-            ("delivery", "Delivered"),
-            ("bounce", "Bounced"),
-            ("spamcomplaint", "Spam Complaint"),
-            ("click", "Link Clicked"),
-            ("subscriptionchange", "Subscription Changed"),
-        ],
-        string="Mail message's state",
-        help="Shows the state of mail messages according to postmark api",
-        readonly=True,
-    )
-
-    def message_format(self):
-        """
-        Frontend'de mail verilerini render eden fonksiyon için state field'ı eklendi.
-        """
-        result = super().message_format()
-        postmark_states = dict(self._fields["postmark_api_state"].selection)
-        for message in result:
-            mail = self.filtered(lambda m: m.id == message["id"])
-            if mail.postmark_api_state:
-                message["state"] = _(postmark_states[mail.postmark_api_state])
-            else:
-                message["state"] = False
-        return result
+    sent_date = fields.Datetime(copy=False)
+    postmark_message_id = fields.Char(copy=False)
