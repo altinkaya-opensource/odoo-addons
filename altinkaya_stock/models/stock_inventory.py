e43831a655873e61ce6a048fc008565b1ddb5305
yibudak <yigit@altinkaya.com>
2025-02-17 11:07:53 +0000

    [REF] Stock Inventory negative qty functionality ported to 16.0

--- altinkaya_stock/models/stock_inventory.py
+++ altinkaya_stock/models/stock_inventory.py
@@ -1,43 +1,21 @@
 # Copyright 2024 Yiğit Budak (https://github.com/yibudak)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from odoo import models, api
+from odoo import fields, models
 
 
 class StockInventory(models.Model):
     _inherit = "stock.inventory"
 
-    @api.model
-    def _selection_filter(self):
-        """
-        Add negative_qty filter
-        :return:
-        """
-        res = super(StockInventory, self)._selection_filter()
-        res.append(("negative_qty", "Negative Quantities"))
-        return res
+    product_selection = fields.Selection(
+        selection_add=[("negative_qty", "Negative Quantities")]
+    )
 
-    def _get_inventory_lines_values(self):
+    def _get_quants(self, locations):
         """
         Filter out positive quantities if negative_qty filter is selected
-        :return:
+        :return: stock.quant
         """
-        res = super(StockInventory, self)._get_inventory_lines_values()
-        if self.filter == "negative_qty":
-            res = [line for line in res if line["product_qty"] < 0]
+        res = super()._get_quants(locations)
+        if self.product_selection == "negative_qty":
+            res = res.filtered(lambda x: x.quantity < 0)
         return res
-
-
-# TODO: Stock Inventory / Stock Inventory Line do not exist in 16.0
-# class StockInventoryLine(models.Model):
-#     _inherit = "stock.inventory.line"
-
-#     @api.constrains("product_id")
-#     def _check_product_id(self):
-#         """
-#         Skip product type on negative_qty filter
-#         :return:
-#         """
-#         if self.inventory_id.filter == "negative_qty":
-#             return True
-#         else:
-#             return super(StockInventoryLine, self)._check_product_id()
