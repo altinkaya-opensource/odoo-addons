f889e011e35ce040a1d834ab12cf3f6bfa3925bb
Ahmet Yigit Budak <yigit@altinkaya.com>
2025-03-25 11:54:04 +0300

    [REF] clean up imports in stock_lot and __init__.py files
    Signed-off-by: Ahmet Yigit Budak <yigit@altinkaya.com>

--- altinkaya_stock_lot/models/stock_lot.py
+++ altinkaya_stock_lot/models/stock_lot.py
@@ -1,8 +1,8 @@
 # Copyright 2022 Yiğit Budak (https://github.com/yibudak)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from odoo import fields, models, api
 import random
-import string
+
+from odoo import fields, models
 
 
 class StockProductionLot(models.Model):
--- product_label_print/models/__init__.py
+++ product_label_print/models/__init__.py
@@ -2,4 +2,3 @@ from . import product
 from . import stock
 from . import printing
 from . import stock_lot
-from . import mrp_production
