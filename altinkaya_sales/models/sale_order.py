b15382bda774bfc47f54dcab420d3cd130e4e33a
erlinberg <develierol@gmail.com>
2025-03-29 18:58:22 +0300

    [FIX] Fix mail.template and sale.report (WIP)

--- altinkaya_sales/models/sale_order.py
+++ altinkaya_sales/models/sale_order.py
@@ -295,7 +295,7 @@ class SaleOrder(models.Model):
         ir_model_data = self.env["ir.model.data"]
         try:
             template_id = ir_model_data.check_object_reference(
-                "altinkaya_sales", "email_template_edi_sale_altinkaya"
+                "altinkaya_sales", "email_template_edi_sale_altinkaya1"
             )[1]
         except ValueError:
             template_id = False
