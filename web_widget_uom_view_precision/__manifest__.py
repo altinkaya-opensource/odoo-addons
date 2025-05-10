fbd64983158b56dfd0372ca48fab74c9b879620f
Ahmet Yigit Budak <yigit@altinkaya.com>
2025-05-10 16:17:13 +0300

    [MIG] web_widget_uom_view_precision: Migrated to 16.0

--- web_widget_uom_view_precision/__manifest__.py
+++ web_widget_uom_view_precision/__manifest__.py
@@ -1,22 +1,20 @@
 {
-    'name': "UOM View Precision Widget",
-
-    'summary': """
+    "name": "UOM View Precision Widget",
+    "summary": """
         This module allows to set the precision of the UOM in the view.
     """,
-
-    "author": "Yiğit Budak, "
-              "Odoo Community Association (OCA)",
-    'website': "https://github.com/oca/web",
-
+    "author": "Ahmet Yiğit Budak, Altinkaya Enclosures",
+    "website": "https://github.com/altinkaya-opensource/odoo-addons",
     "license": "AGPL-3",
     "category": "Web",
-    'version': '12.0.1.0.0',
-
-    'depends': ['base', 'stock', 'web'],
-
-    'data': [
-        'views/web_widget_uom_precision_view.xml',
-        'views/uom_uom_view.xml',
-    ]
+    "version": "16.0.1.0.0",
+    "depends": ["base", "stock", "web"],
+    "data": [
+        "views/uom_uom_view.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "web_widget_uom_view_precision/static/src/js/uom_widget.js",
+        ]
+    },
 }
--- web_widget_uom_view_precision/models/uom_view_precision_backend.py
+++ web_widget_uom_view_precision/models/uom_view_precision_backend.py
@@ -2,9 +2,8 @@ from odoo import api, models
 
 
 class UomViewPrecisionBackend(models.AbstractModel):
-
-    _name = 'uom.view.precision.backend'
-    _description = 'Uom View Precision Backend'
+    _name = "uom.view.precision.backend"
+    _description = "Uom View Precision Backend"
 
     @api.model
     def get_uom_view_precisions(self):
@@ -12,8 +11,8 @@ class UomViewPrecisionBackend(models.AbstractModel):
         This method returns the precision data from uom.uom model.
         :return: dictionary
         """
-        uoms = self.env['uom.uom'].search([])
+        uoms = self.env["uom.uom"].search([])
         if uoms:
-            return {u['id']: u['view_precision'] for u in uoms}
+            return {u["id"]: u["view_precision"] for u in uoms}
         else:
             return {}
