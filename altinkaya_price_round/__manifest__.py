4d834070de9773e9e7d257d000045bc937c01614
Ahmet Yigit Budak <yigit@altinkaya.com>
2025-05-17 11:47:07 +0300

    [ADD] altinkaya_price_round

---
+++ altinkaya_price_round/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Yiğit Budak (https://github.com/yibudak)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Altinkaya Price Round",
+    "summary": "Round prices to 2 decimal places",
+    "version": "16.0.1.0.0",
+    "author": "Yiğit Budak, Altinkaya Enclosures",
+    "website": "https://github.com/altinkaya-opensource/odoo-addons",
+    "license": "LGPL-3",
+    "category": "Tools",
+    "depends": ["base", "sale", "account"],
+    "data": [],
+    "installable": True,
+}
---
+++ altinkaya_price_round/models/account_move.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2025 Ahmet Yiğit Budak (https://github.com/yibudak)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from odoo import api, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    @api.depends(
+        "line_ids.matched_debit_ids.debit_move_id.move_id.payment_id.is_matched",
+        "line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual",
+        "line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual_currency",
+        "line_ids.matched_credit_ids.credit_move_id.move_id.payment_id.is_matched",
+        "line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual",
+        "line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual_currency",
+        "line_ids.balance",
+        "line_ids.currency_id",
+        "line_ids.amount_currency",
+        "line_ids.amount_residual",
+        "line_ids.amount_residual_currency",
+        "line_ids.payment_id.state",
+        "line_ids.full_reconcile_id",
+        "state",
+    )
+    def _compute_amount(self):
+        super()._compute_amount()
+        for move in self:
+            move.amount_total = round(move.amount_total, 2)
+            move.amount_total_signed = round(move.amount_total_signed, 2)
+            move.amount_total_in_currency_signed = round(
+                move.amount_total_in_currency_signed, 2
+            )
