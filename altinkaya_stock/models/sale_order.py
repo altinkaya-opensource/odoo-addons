4c41c207a688a3df3273cda210e508afbba7c431
Ahmet Yigit Budak <yigit@altinkaya.com>
2025-07-09 17:00:06 +0300

    [IMP] stock: added package information on sale order

--- altinkaya_stock/models/sale_order.py
+++ altinkaya_stock/models/sale_order.py
@@ -12,11 +12,27 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from odoo import models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    packed = fields.Boolean(
+        help="Indicates if the sale order has been packed.",
+        default=False,
+        compute="_compute_packed",
+        store=True,
+    )
+
+    @api.depends("picking_ids.package_ids")
+    def _compute_packed(self):
+        """
+        Compute the 'packed' field based on the presence of packages in the picking_ids.
+        If any picking has packages, set packed to True, otherwise False.
+        """
+        for order in self:
+            order.packed = any(picking.package_ids for picking in order.picking_ids)
 
     def _action_cancel(self):
         """
