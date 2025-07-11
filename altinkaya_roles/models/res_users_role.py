bccc915a1216671edb0c490b0bab2ea8a37a882b
Ismail Cagan Yilmaz <ismail.cagan.yilmaz@gmail.com>
2025-07-11 10:41:01 +0300

    [FIX] view and compute fields for users and roles

--- altinkaya_roles/models/res_users_role.py
+++ altinkaya_roles/models/res_users_role.py
@@ -7,39 +7,23 @@ from odoo import api, fields, models
 class ResUsersRole(models.Model):
     _inherit = "res.users.role"
 
-    model_access = fields.One2many(
-        "ir.model.access", string="Access Controls", compute="_compute_model_access"
+    menu_ids = fields.Many2many(
+        "ir.ui.menu", compute="_compute_menu_ids", string="Menu Access"
     )
-    rule_groups = fields.Many2many(
-        "ir.rule", string="Rules", compute="_compute_rule_groups"
-    )
-    menu_access = fields.Many2many(
-        "ir.ui.menu", string="Access Menu", compute="_compute_menu_access"
-    )
-    view_access = fields.Many2many(
-        "ir.ui.view", string="Views", compute="_compute_view_access"
+    view_ids = fields.Many2many(
+        "ir.ui.view", compute="_compute_view_ids", string="View Access"
     )
 
-    @api.depends("group_id")
-    def _compute_model_access(self):
-        self.model_access = False
-        for g in self.group_id:
-            self.model_access |= g.model_access
+    @api.depends("implied_ids")
+    def _compute_menu_ids(self):
+        for role in self:
+            role.menu_ids = self.env["ir.ui.menu"].search(
+                [("groups_id", "in", role.implied_ids.ids)]
+            )
 
-    @api.depends("group_id")
-    def _compute_rule_groups(self):
-        self.rule_groups = False
-        for g in self.group_id:
-            self.rule_groups |= g.rule_groups
-
-    @api.depends("group_id")
-    def _compute_menu_access(self):
-        self.menu_access = False
-        for g in self.group_id:
-            self.menu_access |= g.menu_access
-
-    @api.depends("group_id")
-    def _compute_view_access(self):
-        self.view_access = False
-        for g in self.group_id:
-            self.view_access |= g.view_access
+    @api.depends("implied_ids")
+    def _compute_view_ids(self):
+        for role in self:
+            role.view_ids = self.env["ir.ui.view"].search(
+                [("groups_id", "in", role.implied_ids.ids)]
+            )
