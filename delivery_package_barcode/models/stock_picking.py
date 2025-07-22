37d61d7ce391c6c38220477e3412d3665b2ef0af
Ahmet Yigit Budak <yigit@altinkaya.com>
2025-07-22 15:12:11 +0300

    [IMP] delivery: auto-invoicing search function

--- delivery_package_barcode/models/stock_picking.py
+++ delivery_package_barcode/models/stock_picking.py
@@ -1,6 +1,7 @@
-# Copyright 2025 Yiğit Budak, Ümithan Güldemir (https://github.com/yibudak) (https://github.com/umithan-guldemir)
+# Copyright 2025 Yiğit Budak (https://github.com/yibudak).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import _, fields, models
+from odoo.exceptions import ValidationError
 
 
 class StockPicking(models.Model):
@@ -9,14 +10,31 @@ class StockPicking(models.Model):
 
     block_autoinvoicing = fields.Boolean(
         compute="_compute_block_autoinvoicing",
+        search="_search_block_autoinvoicing",
         help="If True, the autoinvoicing will be blocked for this picking.",
     )
 
+    def _block_autoinvoicing_domain(self):
+        return [
+            "|",
+            ("partner_id.commercial_partner_id.block_autoinvoicing", "=", True),
+            ("sale_id.block_autoinvoicing", "=", True),
+        ]
+
     def _compute_block_autoinvoicing(self):
-        for record in self:
-            commercial_partner = record.partner_id.commercial_partner_id
-            sale_id = record.sale_id
-            if commercial_partner.block_autoinvoicing or sale_id.block_autoinvoicing:
-                record.block_autoinvoicing = True
-            else:
-                record.block_autoinvoicing = False
+        for picking in self:
+            picking.block_autoinvoicing = (
+                picking.partner_id.commercial_partner_id.block_autoinvoicing
+                or picking.sale_id.block_autoinvoicing
+            )
+
+    def _search_block_autoinvoicing(self, operator, value):
+        if operator not in ("=", "!="):
+            raise ValidationError(
+                _("Unsupported operator for Block Autoinvoicing search")
+            )
+        domain = self._block_autoinvoicing_domain()
+        if (operator == "=" and value) or (operator == "!=" and not value):
+            return domain
+        else:
+            return ["!", *domain]
