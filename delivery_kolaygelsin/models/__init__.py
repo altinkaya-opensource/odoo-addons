23c0d38b74b69235a81b03f755ba552bd8e249f8
Ahmet Yiğit Budak <yigit@altinkaya.com>
2026-01-23 15:51:32 +0300

    [FIX] delete unused model in delivery_kolaygelsin
    Signed-off-by: Ahmet Yiğit Budak <yigit@altinkaya.com>

--- delivery_kolaygelsin/models/__init__.py
+++ delivery_kolaygelsin/models/__init__.py
@@ -2,5 +2,4 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # from . import address_district
 from . import delivery_carrier
-from . import product_packaging
 from . import kolaygelsin_request
