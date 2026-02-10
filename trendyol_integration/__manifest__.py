1ccfefbed9542a34788654face475dc2d784bb4d
Ahmet Yiğit Budak <yigit@altinkaya.com>
2026-02-10 17:28:24 +0300

    [FIX] author name

--- trendyol_integration/__manifest__.py
+++ trendyol_integration/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Altinkaya Enclosures
+# Copyright 2026 Ahmet Yigit Budak (https://github.com/yibudak)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
@@ -6,7 +6,7 @@
     "version": "16.0.1.0.0",
     "category": "Sales/Sales",
     "summary": "Integrate Odoo with Trendyol marketplace",
-    "author": "Altinkaya Enclosures",
+    "author": "Ahmet Yigit Budak, Altinkaya Enclosures",
     "website": "https://github.com/altinkaya-opensource/odoo-addons",
     "license": "LGPL-3",
     "depends": [
--- trendyol_integration/controllers/webhook.py
+++ trendyol_integration/controllers/webhook.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Altinkaya Enclosures
+# Copyright 2026 Ahmet Yigit Budak (https://github.com/yibudak)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 import hashlib
