65e69a2b420e6e8e4197b07add632d3257478b0e
Ahmet Yiğit Budak <yigit@altinkaya.com>
2026-03-02 12:21:39 +0300

    [#VSP] update invoice count calculation and perf. optimization
    Signed-off-by: Ahmet Yiğit Budak <yigit@altinkaya.com>

--- altinkaya_reports/report/account_invoice_report.py
+++ altinkaya_reports/report/account_invoice_report.py
@@ -1,8 +1,20 @@
 from odoo import fields, models
+from odoo.tools import sql
 
 
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
+
+    def init(self):
+        res = super().init()
+        sql.create_index(
+            self._cr,
+            "account_move_line_product_move_min_idx",
+            "account_move_line",
+            ["move_id", "id"],
+            where="display_type = 'product' AND account_id IS NOT NULL",
+        )
+        return res
 
     acquirer_id = fields.Many2one(
         "res.partner",
@@ -146,7 +158,9 @@ class AccountInvoiceReport(models.Model):
                line.laser_marking_price as laser_marking_price,
                line.lasercut_price as lasercut_price,
                line.insert_installation_price as insert_installation_price,
-               inv_count_sub.invoice_count,
+               CASE WHEN first_line.move_id IS NOT NULL
+                    THEN 1 ELSE 0
+               END AS invoice_count,
                partner.industry_id as industry_id,
                line.origin_price_usd * line.quantity as origin_price_usd,
                CASE
@@ -198,12 +212,11 @@ class AccountInvoiceReport(models.Model):
             ) scl ON scl.move_line_id = line.id
 
             LEFT JOIN (
-                SELECT
-                    line.id AS invoice_line_id,
-                    COUNT(DISTINCT move.id) AS invoice_count
-                FROM account_move_line line
-                JOIN account_move move ON move.id = line.move_id
-                GROUP BY line.id
-            ) inv_count_sub ON inv_count_sub.invoice_line_id = line.id
+                SELECT move_id, MIN(id) AS first_line_id
+                FROM account_move_line
+                WHERE display_type = 'product'
+                    AND account_id IS NOT NULL
+                GROUP BY move_id
+            ) first_line ON first_line.first_line_id = line.id
             """
         )
