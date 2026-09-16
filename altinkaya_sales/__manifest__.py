097f8fcfaeea45b3dfee565e65cfba7d61ebf1ba
Yiğit Budak <yigit@altinkaya.com>
2026-09-16 10:26:23 +0300

    feat: add category cut-to-order flag

--- altinkaya_sales/__manifest__.py
+++ altinkaya_sales/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Altinkaya Sale Order Extensions",
-    "version": "16.0.1.1.0",
+    "version": "16.0.1.2.0",
     "category": "General",
     "depends": [
         "base",
--- altinkaya_sales/models/product_category.py
+++ altinkaya_sales/models/product_category.py
@@ -11,3 +11,7 @@ class ProductCategory(models.Model):
     _inherit = "product.category"
 
     custom_products = fields.Boolean()
+    cut_to_order = fields.Boolean(
+        string="Cut to Order",
+        help="Products assigned to this category are cut to order.",
+    )
