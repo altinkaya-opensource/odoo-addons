f3ad308cbe1717e0e3f849955babf01fa6586103
Burak Kaan Alkan <bkaanalkan@gmail.com>
2026-09-23 11:59:08 +0300

    [IMP] altinkaya_stock: show warehouse note in operations

--- altinkaya_stock/__manifest__.py
+++ altinkaya_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Altinkaya Stock",
-    "version": "16.0.0.1.1",
+    "version": "16.0.0.1.2",
     "website": "https://github.com/altinkaya-opensource/odoo-addons",
     "license": "LGPL-3",
     "author": "Ahmet Altınışık,OnurUgur,Codequarters,Yavuz Avcı,Altinkaya Enclosures",
--- altinkaya_stock/tests/__init__.py
+++ altinkaya_stock/tests/__init__.py
@@ -1,2 +1,3 @@
 from . import test_stock_quant
+from . import test_stock_picking_view
 from . import test_stock_inventory
